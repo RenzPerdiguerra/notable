04f758d76e32318044aa207b657fda14503188aa
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2026-07-31 21:56:19 +0800

    refactor(build): vite-friendly setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import risci_pic from './assets/RISCI_GradPic (2018).JPG';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={risci_pic} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
