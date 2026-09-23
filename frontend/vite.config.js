676ee60c6bd11862422791738f5dacfbc582d9ee
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2026-09-24 07:36:35 +0800

    refactor(fe/test): removed unused files, add scripts to package.json and fix setupFiles path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ".src/tests/setup.js",
+    setupFiles: "src/tests/setup.js",
     coverage: {
       provider: "v8",
       reporter: ["text", "html"],
